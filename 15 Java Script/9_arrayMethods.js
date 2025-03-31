83a28fb68498bdb3b246aec7a31b660c6edb2661
srinjoymahanti <srinjoymahanti@gmail.com>
2025-03-31 15:14:34 +0530

    array method filter

--- 15 Java Script/9_arrayMethods.js
+++ 15 Java Script/9_arrayMethods.js
@@ -33,7 +33,15 @@
 // })
 
 
-let arr=[1,2,3,4,5];
-let double=arr.map((el)=>{
-    console.log( el*2);
-});
+// let arr=[1,2,3,4,5];
+// let double=arr.map((el)=>{
+//     console.log( el*2);
+// });
+
+// let arr=[1,2,3,4,5,6,7,8,9,10,11,12];
+// let even=arr.filter((el)=>{
+//     return el%2 == 0;
+// })
+// console.log(even);
+
+

d5e6072eeb395868208ad7715cf25106c8bc1a62
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-01 19:24:39 +0530

    destructuring

--- 15 Java Script/9_arrayMethods.js
+++ 15 Java Script/9_arrayMethods.js
@@ -79,6 +79,46 @@
 // console.log(result);
 
 
-let arr=[1,2,9,3,4,7,6,5,11];
-let minimum=Math.min(...arr);//takes every elements from array
-console.log(minimum);
+// let arr=[1,2,9,3,4,7,6,5,11];
+// let minimum=Math.min(...arr);//takes every elements from array
+// console.log(minimum);
+
+// let char=[..."hello"];
+// console.log(char);
+// let even=[2,4,6,8,10];
+// let odd=[1,3,5,7,9];
+// let num=[...even,...odd];
+// console.log(num);
+
+
+// const data={
+//     name:"srinjoy",
+//     age:21,
+// };
+// const dataCopy={...data,id:123,password:"abcd"};
+// console.log(dataCopy);
+
+
+// function arr(){
+//     console.log(arguments);
+//     console.log(arguments.length);
+// }
+// function arr(...args){
+//     return args.reduce((sum,el)=>sum+el);
+// }
+
+
+// let names=["tony","bruce","steve","peter"];
+// let [winner,runnersup,...others]=names;
+// console.log(winner)
+// console.log(runnersup)
+// console.log(others)
+
+
+const students={
+    name:"arya",
+    age:21,
+    city:"kolkata",
+    marks:94,
+};
+let{name:nam,city:place}=students;

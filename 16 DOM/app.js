848dd2e60c5f8ef12f975e86e2d3e02ef5432f98
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-02 23:57:13 +0530

    get elements by class name

--- 16 DOM/app.js
+++ 16 DOM/app.js
@@ -1,2 +1,8 @@
 // console.log(document.getElementsByTagName("p"));
 // document.getElementsByTagName("p")[1].innerText="lorem";
+
+
+// console.log(document.getElementById("mainImg"));
+// console.log(document.getElementById("mainImg").id);
+// console.log(document.getElementById("mainImg").tagName);
+// console.log(document.getElementById("mainImg").src);

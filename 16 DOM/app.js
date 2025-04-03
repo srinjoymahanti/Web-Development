4c93127478449fafcc5dacef621f14769b226883
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-03 09:44:21 +0530

    get elements by class name

--- 16 DOM/app.js
+++ 16 DOM/app.js
@@ -6,3 +6,11 @@
 // console.log(document.getElementById("mainImg").id);
 // console.log(document.getElementById("mainImg").tagName);
 // console.log(document.getElementById("mainImg").src);
+
+
+// console.log(document.getElementsByClassName("oldImages"));
+// let smallImages=document.getElementsByClassName("oldImages");
+// for(let i=0;i<smallImages.length;i++){
+//     smallImages[i].src="Assets/Spider-Man.png";
+//     console.log(`value of image no${i} is changed`);
+// };

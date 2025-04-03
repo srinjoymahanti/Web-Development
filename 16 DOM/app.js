fb200b4425514ce36a75deae3aa8b30bc73853f4
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-03 23:51:56 +0530

    navigation on page

--- 16 DOM/app.js
+++ 16 DOM/app.js
@@ -55,4 +55,18 @@
 // console.log(heading.classList);
 
 
+// let h4=document.querySelector("h4");
+// console.log(h4.parentElement);
+// console.log(h4.children);
+// let ul=document.querySelector("ul");
+// console.log(ul.children);
+// console.log(ul.children[0]);
+// console.log(ul.children[1]);
+// console.log(ul.children[2]);
+// let image=document.querySelector("img");
+// console.log(image.previousElementSibling);
+// console.log(image.nextElementSibling);
+// image.nextElementSibling.style.color="red";
+
+
 

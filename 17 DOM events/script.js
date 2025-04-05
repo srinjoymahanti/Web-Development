b3c2370021b8827956743d084355c4c8c61d30fc
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-05 17:25:02 +0530

    onclick function

---
+++ 17 DOM events/script.js
@@ -0,0 +1,13 @@
+// let btns=document.querySelectorAll("button");
+// for(btn of btns){
+//     btn.onclick=click;//when a button is clicked
+//     btn.onmouseenter=hover;//when cursor is hovered over a button
+// }
+// function click(){
+//     alert("You clicked the button");
+// }
+// function hover(){
+//     console.log("You hover over the button");
+// };
+
+

b510d83906e529d23c921d48153ebf5ec6ed0a7f
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-06 23:24:16 +0530

    keyboard events

--- 17 DOM events/script.js
+++ 17 DOM events/script.js
@@ -47,3 +47,24 @@
 // btn.addEventListener("click",changeColor);
 
 
+// let inp=document.querySelector("input");
+// // inp.addEventListener("keydown",function(){
+// //     console.log("you pressed the key");
+// // })
+// inp.addEventListener("keydown",function(event){
+//     console.log("code=",event.code);
+//     if(event.code=="ArrowUp"){
+//         console.log("character moves forward");
+//     }
+//     else if(event.code=="ArrowDown"){
+//         console.log("character moves backward");
+//     }
+//     else if(event.code=="ArrowLeft"){
+//         console.log("character moves left");
+//     }
+//     else if(event.code=="ArrowRight"){
+//         console.log("character moves right");
+//     }
+// });
+
+

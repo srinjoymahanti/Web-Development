1fed2c8b85c342089833a5b6b9edb9337fbf0bc5
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-06 09:28:40 +0530

    get random color

--- 18 Activity/script.js
+++ 18 Activity/script.js
@@ -1,1 +1,18 @@
-let btn=document
+let btn=document.querySelector("button");
+function getRandomColor(){
+    let red=Math.floor(Math.random() * 255);
+    let green=Math.floor(Math.random() * 255);
+    let blue=Math.floor(Math.random() * 255);
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
+
+btn.addEventListener("click",function(){
+    let h3=document.querySelector("h3");
+    let randomColor=getRandomColor();
+    h3.innerText=randomColor;
+
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomColor;
+    div.innerText="This is a new color";
+});

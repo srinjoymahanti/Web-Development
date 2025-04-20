00843d47ec6320e64c895322027f4b9a1a1d8dd5
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-20 09:51:48 +0530

    modified again

--- 21 Call Stack/script.js
+++ 21 Call Stack/script.js
@@ -148,3 +148,22 @@
 //     await getNum();
 //     getNum();
 // }
+
+
+// let h1=document.querySelector("h1");
+// function colorChange(color,delay){
+//     return new Promise((resolve,reject) =>{
+//         setTimeout(()=>{
+//             h1.style.color=color;
+//             console.log(`Color changed to ${color}`);
+//             resolve("Color Changed!");
+//         },delay);
+//     }) 
+// };
+// async function demo() {
+//     await colorChange("red",1000);
+//     await colorChange("blue",1000);
+//     await colorChange("yellow",1000);
+//     await colorChange("green",1000);
+//     colorChange("orange",1000);
+// }

23098d7d53955208ecfae1350a333ad308feae0d
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-20 22:51:47 +0530

    modify using async function

--- 22 API/script.js
+++ 22 API/script.js
@@ -16,3 +16,19 @@
 // .catch((err)=>{
 //     console.log("ERROR-",err);
 // })
+
+
+let url="https://catfact.ninja/fact";
+async function getFacts(){
+    try{
+        let res=await fetch(url);
+        let data=await res.json();
+        console.log(data.fact);
+
+        let res2=await fetch(url);
+        let data2=await res2.json();
+        console.log(data2.fact);
+    }catch(err){
+        console.log("error:",err);
+    }
+}

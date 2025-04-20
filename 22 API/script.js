98b0b16580c669c12fcc867b4640bfa929bf48a6
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-20 22:43:35 +0530

    API calling

---
+++ 22 API/script.js
@@ -0,0 +1,18 @@
+// let url="https://catfact.ninja/fact";
+// fetch(url)
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data)=>{
+//     console.log("Data 1=",data.fact);
+//     return fetch(url);
+// })
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data)=>{
+//     console.log("Data 2=",data.fact);
+// })
+// .catch((err)=>{
+//     console.log("ERROR-",err);
+// })

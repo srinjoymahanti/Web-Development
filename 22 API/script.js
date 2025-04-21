f347f9af0c4b7db32b6093381c0cc4a1aa5f9ddc
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-21 21:08:54 +0530

    axios using

--- 22 API/script.js
+++ 22 API/script.js
@@ -18,17 +18,36 @@
 // })
 
 
+let btn=document.querySelector("#button1");
+btn.addEventListener("click",async ()=>{
+    let fact=await getFacts();
+    let para=document.querySelector("p");
+    para.innerText=fact;
+})
 let url="https://catfact.ninja/fact";
 async function getFacts(){
     try{
-        let res=await fetch(url);
-        let data=await res.json();
-        console.log(data.fact);
-
-        let res2=await fetch(url);
-        let data2=await res2.json();
-        console.log(data2.fact);
+        let res=await axios.get(url);
+        return res.data.fact;
     }catch(err){
         console.log("error:",err);
+        return "No fact found";
+    }
+}
+
+let btn2=document.querySelector("#button2");
+btn2.addEventListener("click",async ()=>{
+    let link=await getImage();
+    let img=document.querySelector("img");
+    img.setAttribute("src",link);
+})
+let url2="https://dog.ceo/api/breeds/image/random";
+async function getImage(){
+    try{
+        let res=await axios.get(url2);
+        return res.data.message;
+    }catch(err){
+        console.log("error:",err);
+        return "No image found";
     }
 }

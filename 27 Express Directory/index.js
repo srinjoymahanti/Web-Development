9a48504f16c2403a01915ed9ce5fbfc9ec751cdc
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-23 17:17:17 +0530

    get function in express

--- 27 Express Directory/index.js
+++ 27 Express Directory/index.js
@@ -1,7 +1,14 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-let port=8080;
-app.listen(port,()=>{
-    console.log(`app is listening on port ${port}`);
-})
+let port =8080;
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
+
+
+// app.use((req,res)=>{
+//     console.log("request recieved");
+//     res.send("<h1>Fruits</h1> <ol><li>apple</li><li>orange</li>");
+// });

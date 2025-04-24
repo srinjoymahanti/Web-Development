a3ed681b76b51862924c64f787beb54be89c9891
srinjoymahanti <srinjoymahanti@gmail.com>
2025-04-24 19:14:09 +0530

    some changes

---
+++ 24 Node JS/script.js
@@ -0,0 +1,18 @@
+// let n=5;
+// for (let i=0;i<n;i++){
+//     console.log("hello",i);
+// };
+
+
+// let args=process.argv;
+// for (let i=2;i<args.length;i++){
+//     console.log("Hello to",args[i]);
+// }
+
+// const math=require("./math");
+// console.log(math.sum(2,2));
+// console.log(math.pi);
+
+
+const info=require("./fruits");
+console.log(info[0].name);

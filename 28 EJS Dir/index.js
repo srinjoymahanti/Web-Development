8f0c6f0d56152ea6dc790cbf9f3c073e97cadc26
srinjoymahanti <srinjoymahanti@gmail.com>
2025-05-09 16:16:56 +0530

    insta pages with ejs

--- 28 EJS Dir/index.js
+++ 28 EJS Dir/index.js
@@ -14,19 +14,29 @@ app.listen(port,()=>{
 app.get("/", (req, res) => {
     res.render("home.ejs", { user: "Srinjoy"  });
   });
+
+
 app.get("/rollDice", (req, res) => {
     let diceVal= Math.ceil(Math.random()*6);
     res.render("rollDice.ejs",{num:diceVal});
 });
-app.get("/users/:name",(req,res)=>{
+
+
+app.get("/users/:username",(req,res)=>{
     const followers=["adam","bob","steve","john"];
     let {username}=req.params;
     res.render("users.ejs",{username,followers});
 })
-app.get("/instagram/:username",(req,res)=>{
-    let {username}=req.params;
+
+
+app.get("/instagram/:name",(req,res)=>{
+    let username=req.params.name;
     const instaData=require("./data.json");
     const data=instaData[username];
     console.log(data);
-    res.render("instagram.ejs",{data});
+    if(data){
+        res.render("instagram.ejs",{data});
+    }else{
+        res.render("error.ejs",{username});
+    }
 })

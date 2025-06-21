48e07d7c96eeed90befd7da507f24b368b0f4cf9
srinjoymahanti <srinjoymahanti@gmail.com>
2025-06-21 22:03:33 +0530

    using constructors

--- 29 Miscellaneous/Frontend/app.js
+++ 29 Miscellaneous/Frontend/app.js
@@ -1,10 +1,23 @@
-function Personmaker(name,age){
-    const person={
-        name:name,
-        age:age,
-        talk(){
-            console.log(`Hi,my name is ${this.name}`);
-        },
-    };
-    return person;
-}
+// //Factory functions
+// function Personmaker(name,age){
+//     const person={
+//         name:name,
+//         age:age,
+//         talk(){
+//             console.log(`Hi,my name is ${this.name}`);
+//         },
+//     };
+//     return person;
+// }
+
+// //Constructors->doesn't return anything and starts with capital
+// function Person(name,age){
+//     this.name=name;
+//     this.age=age;
+//     console.log(this);
+// }
+// Person.prototype.talk=function(){
+//     console.log(`Hi my name is ${this.name}`);
+// };
+// let p1=new Person("adam",25);
+// let p2=new Person("eve",25);

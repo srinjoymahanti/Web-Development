80c7bee96f2d565f31d3e7fac914ae52305d2533
srinjoymahanti <srinjoymahanti@gmail.com>
2025-09-14 20:47:15 +0530

    changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Food from './Food.jsx';
-
 import Card from './Card.jsx';
 import Button from './Button.jsx';
 import Student from './Student.jsx' 

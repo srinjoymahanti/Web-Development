92808e049692daef6f7589dee86ad6afb94610a3
Srinjoy <srinjoymahanti@gmail.com>
2025-10-14 16:53:50 +0530

    creating listing

--- 34 wanderlust/app.js
+++ 34 wanderlust/app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const mongoose=require("mongoose");
+const Listing=require("..models/listings.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,6 +18,18 @@ async function main(){
 }
 app.get("/",(req,res)=>{
     res.send("i am root");
+})
+app.get("/testListing",async(req,res)=>{
+    let sampleListing=new Listing({
+        title:"my villa",
+        description:"by the beach",
+        price:1200,
+        location:"Goa",
+        country:"India",
+    });
+    await sampleListing.save();
+    console.log("sample is saved");
+    res.send("successful testing");
 })
 app.listen(8080, ()=>{
     console.log("server is listening to port 8080");

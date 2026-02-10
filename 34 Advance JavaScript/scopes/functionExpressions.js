05d3f2c3b1f2aa6e0ee552377e17f3ac17e63bcd
Srinjoy <srinjoymahanti@gmail.com>
2026-02-10 22:08:43 +0530

    add function expressions and variable scope examples

---
+++ 34 Advance JavaScript/scopes/functionExpressions.js
@@ -0,0 +1,15 @@
+const myFun1 = function fun(x) { // named function expression
+ console.log("calling...", x);
+}
+// const myFun2 = function (x) { // anonymous function expression
+//  console.log("calling...", x);
+// }
+// const myFun3 = () => { // arrow function / arrow function expression
+//  console.log("calling...", x);
+//}
+// (function fun(x) { // IIFE - Immediately invoked function expression
+//  console.log("calling...", x);
+// })(10)
+
+myFun1(10);
+

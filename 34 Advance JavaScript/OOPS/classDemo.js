82ac0fab49f094a34babfed401950e53eab1eab3
Srinjoy <srinjoymahanti@gmail.com>
2026-02-11 00:36:30 +0530

    fix class name casing and enable inline suggestions in settings

--- 34 Advance JavaScript/OOPS/classDemo.js
+++ 34 Advance JavaScript/OOPS/classDemo.js
@@ -1,4 +1,16 @@
-class product{
+class Product{
+    // name;
+    // price;
+    // category;
+    // description;
+    // rating;
+    constructor(productName, productPrice, productCategory, productDescription, productRating) {
+        this.name = productName;
+        this.price=productPrice;
+        this.category = productCategory;
+        this.description = productDescription;
+        this.rating = productRating;
+    }
     addToCart(){
         console.log("added to cart");
     }
@@ -8,10 +20,14 @@ class product{
     displayProduct(){
         console.log("product is displayed");
     }
+
 }
 
-console.dir(product);
+// console.dir(product);
 
-let iphone = new product();
+// let iphone = new product();
 
-console.dir(iphone);
+// console.dir(iphone);
+let iphone = new Product("Iphone 11", 900, "Electronics", "Apple Iphone 11", 4.5);
+
+console.log(iphone);

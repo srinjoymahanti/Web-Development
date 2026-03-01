b614eb585d0a2f887dd602fedae0233e4266175e
Srinjoy <srinjoymahanti@gmail.com>
2026-03-01 23:45:46 +0530

    refactor download, write, and upload functions for improved logging and consistency

--- 34 Advance JavaScript/Async Js/downloadWriteUpload.js
+++ 34 Advance JavaScript/Async Js/downloadWriteUpload.js
@@ -1,34 +1,42 @@
-function download(url,cb){
-    console.log("start downloading...",url);
-    setTimeout(()=>{
-        let file = "file content from "+url;
-        console.log("content downloaded");
-        cb(file);
-    },5000)
+function download(url, cb) {
+  console.log("start downloading...", url);
+  setTimeout(() => {
+    let file = "file content from " + url;
+    console.log("content downloaded");
+    cb(file);
+     console.log("value given to the download call back");
+  }, 5000);
 }
 
-function write(data,fileName,cb){
-    console.log("writting in file",fileName);
-    setTimeout(()=>{
-        fileName="changed the file with "+data;
-        console.log("file changed successfully");
-        cb(fileName);
-    },5000);
+function write(data, fileName, cb) {
+  console.log("writting in file", fileName);
+  setTimeout(() => {
+    fileName = "changed the file with " + data;
+    console.log("file changed successfully");
+    cb(fileName);
+    console.log("value given to the write call back");
+  }, 5000);
 }
 
-function upload(file,url,cb){
-    console.log("starting to upload",file," in ",url);
-    setTimeout(()=>{
-        console.log("uploaded successfully");
-        let status="true";
-        cb(status);
-    },5000)
+function upload(file, url, cb) {
+  console.log("starting to upload", file, " in ", url);
+  setTimeout(() => {
+    console.log("uploaded successfully");
+    let status = "true";
+    cb(status);
+     console.log("value given to the upload call back");
+  }, 5000);
 }
 
-download("www.google.com",(txt)=>{
-    write(1212,txt,(txt)=>{
-        upload(txt,"www.microsoft.com",(status)=>{
-            console.log("all tasks done with ",status);
-        })
-    })
-})
+function process(urlLink) {
+    //we can use named function , annonymous function and arrow function also
+  download(urlLink, function handleDownload(txt) {
+    write(1212, txt, function (txt) {
+      upload(txt, "www.microsoft.com", (status) => {
+        console.log("all tasks done with ", status);
+      });
+    });
+  });
+};
+
+process("https://www.google.com");

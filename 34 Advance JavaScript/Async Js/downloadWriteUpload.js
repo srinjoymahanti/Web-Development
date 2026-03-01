f3019cb3b4400c5d252187935173a70220748c65
Srinjoy <srinjoymahanti@gmail.com>
2026-03-02 00:03:51 +0530

    refactor process flow for download, write, and upload functions to improve readability and maintainability

--- 34 Advance JavaScript/Async Js/downloadWriteUpload.js
+++ 34 Advance JavaScript/Async Js/downloadWriteUpload.js
@@ -4,7 +4,7 @@ function download(url, cb) {
     let file = "file content from " + url;
     console.log("content downloaded");
     cb(file);
-     console.log("value given to the download call back");
+    console.log("value given to the download call back");
   }, 5000);
 }
 
@@ -24,19 +24,34 @@ function upload(file, url, cb) {
     console.log("uploaded successfully");
     let status = "true";
     cb(status);
-     console.log("value given to the upload call back");
+    console.log("value given to the upload call back");
   }, 5000);
 }
 
+// function process(urlLink) {
+//     //we can use named function , annonymous function and arrow function also
+//   download(urlLink, function handleDownload(txt) {
+//     write(1212, txt, function (txt) {
+//       upload(txt, "www.microsoft.com", (status) => {
+//         console.log("all tasks done with ", status);
+//       });
+//     });
+//   });
+// };
+
+
 function process(urlLink) {
-    //we can use named function , annonymous function and arrow function also
-  download(urlLink, function handleDownload(txt) {
-    write(1212, txt, function (txt) {
-      upload(txt, "www.microsoft.com", (status) => {
-        console.log("all tasks done with ", status);
-      });
-    });
-  });
-};
+  download(urlLink, handleDownload);
+}
+function handleDownload(txt) {
+  write(1212, txt, handleWrite);
+}
+function handleWrite(txt) {
+  upload(txt, "www.microsoft.com", handleUpload);
+}
+function handleUpload(status) {
+  console.log("all tasks done with ", status);
+}
+
 
 process("https://www.google.com");
